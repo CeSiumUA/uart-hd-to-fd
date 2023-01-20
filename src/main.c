90d7a5f4e9164d21d1fe417e6ea0c970cf6e036d
Fedir Katushonok <fedir.katushonok@gmail.com>
2023-01-20 18:11:54 +0200

    added dma handler labels

--- src/main.c
+++ src/main.c
@@ -24,27 +24,32 @@ int main(void){
     }
 }
 
-
+// USART6 RX
 void DMA2_Stream1_IRQHandler(void){
 
 }
 
+// USART1 RX
 void DMA2_Stream2_IRQHandler(void){
     
 }
 
+// USART6 TX
 void DMA2_Stream6_IRQHandler(void){
     
 }
 
+// USART1 TX
 void DMA2_Stream7_IRQHandler(void){
     
 }
 
+// USART2 RX
 void DMA1_Stream5_IRQHandler(void){
     
 }
 
+// USART2 TX
 void DMA1_Stream6_IRQHandler(void){
     
 }
